3457d7cdda1c4c3963c6a44c247a0d2e824b5aa6
Joaquin Duran <duranjoa20@gmail.com>
2024-05-15 09:27:33 -0300

    Final cleanup

--- CompassChallenge/Model/Model.swift
+++ CompassChallenge/Model/Model.swift
@@ -1,11 +1,6 @@
-// This file was generated from JSON Schema using quicktype, do not modify it directly.
-// To parse the JSON, add this file to your project and do:
-//
-//   let welcome = try? JSONDecoder().decode(Welcome.self, from: jsonData)
-
 import Foundation
 
-// MARK: - WelcomeElement
+
 struct Emoji: Decodable, Hashable {
     let name: String?
     let category: Category?
